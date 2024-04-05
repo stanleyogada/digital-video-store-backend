246df2594e46db37e0d21e73b3ccf6f22a09cac9
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-04-05 22:00:17 +0100

    Have the user model ready

--- src/main/java/com/digitalvideo/store/model/Movie.java
+++ src/main/java/com/digitalvideo/store/model/Movie.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
--- src/main/java/com/digitalvideo/store/model/User.java
+++ src/main/java/com/digitalvideo/store/model/User.java
@@ -1,5 +1,23 @@
 package com.digitalvideo.store.model;
 
-public class User {
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Data
+@Document(collection = "users")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@RequiredArgsConstructor
+public class User {
+  @Id
+  private String id;
+
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
 }
