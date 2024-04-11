994393eb73d4706dcd9e2852938f07747895653f
Stanley Chinedu Ogada <102979724+stanleyogada@users.noreply.github.com>
2024-04-11 12:14:53 +0100

    Delete java-docker-build-run.sh

--- java-docker-build-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull origin main;
-./mvnw clean;
-./mvnw clean install;
-docker container rm -f $(docker container ls -aq);
-docker image build -t chineduogada/digital-video-store .;
-docker push chineduogada/digital-video-store:lastest;
-docker container run -dp 80:8080 --env-file src/main/resources/.env chineduogada/digital-video-store;
